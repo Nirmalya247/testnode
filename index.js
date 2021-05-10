ad6ae47b15c73a14f9fbe3ba6f134e134abf6589
Nirmalya247 <nrmlgn247@gmail.com>
2021-05-10 17:10:24 +0530

    a1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.all('/testweb', testWeb);
+http.createServer(app).listen(4210);
+
+function testWeb(req, res) {
+    res.send('test');
+}
